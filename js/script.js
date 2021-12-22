245715ac197f6c23d973b4ccb5c5e413f34c5d7c
AzaZefir <arykov.azat.kg@gmail.com>
2021-12-21 17:16:42 -0800

    js starter

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// В сценарии создайте две переменные числового типа со значениями 10 и 20. Выведите на экран
+// результаты работы всех операторов сравнения, которые Вы запомнили. 
+let apple = 10;
+let banana = 20;
+
+alert(apple < banana);
+alert(apple > banana);
+alert(apple <= banana);
+alert(apple >= banana);
+alert(apple != banana);
+alert(apple == banana);
+alert(apple === banana);
+alert(apple = banana);
+
